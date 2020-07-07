c1cbaa729081093b95dc90298011a6a986236b35
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2020-07-07 12:00:22 -0300

    acho que agora foi o login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:gerenteloja/blocs/login_bloc.dart';
 import 'package:gerenteloja/widgets/input_field.dart';
 
+import 'home_sreen.dart';
+
 class LoginScreen extends StatefulWidget {
   @override
   _LoginScreenState createState() => _LoginScreenState();
@@ -11,60 +13,99 @@ class _LoginScreenState extends State<LoginScreen> {
 
   final _loginBloc = LoginBloc();
 
+
+  @override
+  void initState() {
+    super.initState();
+    
+    _loginBloc.outState.listen((state) {
+      switch(state){
+        case LoginState.SUCCESS:
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context)=> HomeScreen())
+          );
+          break;
+        case LoginState.FAIL:
+          showDialog(context: context, builder: (context)=>AlertDialog(
+            title: Text("Erro"),
+            content: Text("Você nao possui os privilégios necessários"),
+          ));
+          break;
+        case LoginState.LOADING:
+        case LoginState.IDLE:
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[850],
-      body: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          Container(),
-          SingleChildScrollView(
-            child: Container(
-              margin: EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Icon(
-                    Icons.store_mall_directory,
-                    color: Colors.pinkAccent,
-                    size: 160,
-                  ),
-                  InputField(
-                    icon: Icons.person_outline,
-                    hint: "Usuario",
-                    obscure: false,
-                    stream: _loginBloc.outEmail,
-                    onChanged: _loginBloc.changeEmail,
-                  ),
-                  InputField(
-                    icon: Icons.lock_outline,
-                    hint: "Senha",
-                    obscure: true,
-                    stream: _loginBloc.outPassword,
-                    onChanged: _loginBloc.changePassword,
-                  ),
-                  SizedBox(height: 32,),
-                  StreamBuilder<bool>(
-                    stream: _loginBloc.outSubmitValid,
-                    builder: (context, snapshot) {
-                      return SizedBox(
-                        height: 50,
-                        child: RaisedButton(
+      body: StreamBuilder<LoginState>(
+        stream: _loginBloc.outState,
+        initialData: LoginState.LOADING,
+        // ignore: missing_return
+        builder: (context, snapshot) {
+          switch(snapshot.data){
+            case LoginState.LOADING:
+              return Center(child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation(Colors.pinkAccent),),);
+            case LoginState.FAIL:
+            case LoginState.SUCCESS:
+            case LoginState.IDLE:
+            return Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                Container(),
+                SingleChildScrollView(
+                  child: Container(
+                    margin: EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Icon(
+                          Icons.store_mall_directory,
                           color: Colors.pinkAccent,
-                          child: Text("Entrar"),
-                          onPressed: snapshot.hasData ? _loginBloc.submit : null,
-                          textColor: Colors.white,
-                          disabledColor: Colors.pinkAccent.withAlpha(140),
+                          size: 160,
                         ),
-                      );
-                    }
-                  )
-                ],
-              ),
-            ),
-          ),
-        ],
+                        InputField(
+                          icon: Icons.person_outline,
+                          hint: "Usuario",
+                          obscure: false,
+                          stream: _loginBloc.outEmail,
+                          onChanged: _loginBloc.changeEmail,
+                        ),
+                        InputField(
+                          icon: Icons.lock_outline,
+                          hint: "Senha",
+                          obscure: true,
+                          stream: _loginBloc.outPassword,
+                          onChanged: _loginBloc.changePassword,
+                        ),
+                        SizedBox(height: 32,),
+                        StreamBuilder<bool>(
+                            stream: _loginBloc.outSubmitValid,
+                            builder: (context, snapshot) {
+                              return SizedBox(
+                                height: 50,
+                                child: RaisedButton(
+                                  color: Colors.pinkAccent,
+                                  child: Text("Entrar"),
+                                  onPressed: snapshot.hasData ? _loginBloc.submit : null,
+                                  textColor: Colors.white,
+                                  disabledColor: Colors.pinkAccent.withAlpha(140),
+                                ),
+                              );
+                            }
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            );
+          }
+        }
       ),
     );
   }
