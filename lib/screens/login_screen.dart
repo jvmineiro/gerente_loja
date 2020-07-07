f34d0a7828a1b49014c5d95ce6dbf18b4100a28a
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2020-07-07 12:02:11 -0300

    dispose

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -37,6 +37,13 @@ class _LoginScreenState extends State<LoginScreen> {
     });
   }
 
+
+  @override
+  void dispose() {
+    _loginBloc.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
