c2aec1ffe1564c15f013895227029f1b01156ed3
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2020-07-15 17:50:57 -0300

    filtro de usuarios finalizado

--- lib/tabs/users_tab.dart
+++ lib/tabs/users_tab.dart
@@ -21,6 +21,7 @@ class UsersTab extends StatelessWidget {
               icon: Icon(Icons.search, color: Colors.white,),
               border: InputBorder.none
             ),
+            onChanged: _userBloc.onChangedSearch,
           ),
         ),
         Expanded(
