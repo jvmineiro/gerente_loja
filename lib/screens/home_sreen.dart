3aa18c805b5829971f92c53d80c5b4df423020df
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2020-07-21 14:45:24 -0300

    ordenando pedidos

--- lib/screens/home_sreen.dart
+++ lib/screens/home_sreen.dart
@@ -1,5 +1,6 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:gerenteloja/blocs/orders_bloc.dart';
 import 'package:gerenteloja/blocs/user_bloc.dart';
 import 'package:gerenteloja/tabs/orders_tab.dart';
@@ -93,6 +94,42 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
+      floatingActionButton: _buildFloating(),
     );
   }
+
+  Widget _buildFloating() {
+    switch(_page){
+      case 0:
+        return null;
+      case 1:
+        return SpeedDial(
+          child: Icon(Icons.sort),
+          backgroundColor: Colors.pinkAccent,
+          overlayOpacity: 0.4,
+          overlayColor: Colors.black,
+          children: [
+            SpeedDialChild(
+              child: Icon(Icons.arrow_downward, color: Colors.pinkAccent,),
+              backgroundColor: Colors.white,
+              label: "Concluidos Abaixo",
+              labelStyle: TextStyle(fontSize: 14),
+              onTap: (){
+                _ordersBloc.setOrderCriteria(SortCriteria.READY_LAST);
+              }
+            ),
+            SpeedDialChild(
+              child: Icon(Icons.arrow_upward, color: Colors.pinkAccent,),
+              backgroundColor: Colors.white,
+              label: "Concluidos Acima",
+              labelStyle: TextStyle(fontSize: 14),
+              onTap: (){
+                _ordersBloc.setOrderCriteria(SortCriteria.READY_FIRST);
+              }
+            )
+          ],
+        );
+    }
+  }
+
 }
