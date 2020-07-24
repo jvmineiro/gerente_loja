ffe96fadeca5a34ae4d78af351c62f856e7a52cd
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2020-07-24 14:57:16 -0300

    tela de produtos tomando forma

--- lib/blocs/product_bloc.dart
+++ lib/blocs/product_bloc.dart
@@ -1,18 +1,36 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:rxdart/rxdart.dart';
 
 class ProductBloc extends BlocBase{
+
+  final _dataController = BehaviorSubject<Map>();
+
+  Stream<Map> get outData => _dataController.stream;
 
    String categoryId;
    DocumentSnapshot product;
 
-   ProductBloc({this.categoryId, this.product}){
+   Map<String, dynamic> unsavedData;
 
+   ProductBloc({this.categoryId, this.product}){
+    if(product != null){
+      unsavedData = Map.of(product.data);
+      unsavedData["iamges"] = List.of(product.data["images"]);
+      unsavedData["sizes"] = List.of(product.data["sizes"]);
+    } else {
+      unsavedData = {
+        "title": null, "description": null, "price": null, "images": [],"sizes": [],
+      };
+    }
+    
+    _dataController.add(unsavedData);
    }
 
   @override
   void dispose() {
     // TODO: implement dispose
+    _dataController.close();
   }
 
 }
--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -2,20 +2,42 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:gerenteloja/blocs/product_bloc.dart';
 
-class ProductScreen extends StatelessWidget {
+class ProductScreen extends StatefulWidget {
 
   final String categoryId;
   final DocumentSnapshot product;
+
+  ProductScreen({this.categoryId, this.product});
+
+
+  @override
+  _ProductScreenState createState() => _ProductScreenState(categoryId, product);
+}
+
+class _ProductScreenState extends State<ProductScreen> {
 
   final ProductBloc _productBloc;
 
   final _formKey = GlobalKey<FormState>();
 
-  ProductScreen({this.categoryId, this.product}) :
-    _productBloc = ProductBloc(categoryId: categoryId, product: product);
+  _ProductScreenState(String categoryId, DocumentSnapshot product) :
+        _productBloc = ProductBloc(categoryId: categoryId, product: product);
 
   @override
   Widget build(BuildContext context) {
+
+    InputDecoration _buidlDecoration(String label) {
+      return InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.grey)
+      );
+    }
+
+    final _fieldStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 16,
+    );
+
     return Scaffold(
       backgroundColor: Colors.grey[850],
       appBar: AppBar(
@@ -34,11 +56,43 @@ class ProductScreen extends StatelessWidget {
       ),
       body: Form(
         key: _formKey,
-        child: ListView(
-          padding: EdgeInsets.all(16),
-          children: <Widget>[
+        child: StreamBuilder<Map>(
+          stream: _productBloc.outData,
+          builder: (context, snapshot) {
+            if(!snapshot.hasData) return Container();
+            return ListView(
+              padding: EdgeInsets.all(16),
+              children: <Widget>[
+                TextFormField(
+                  initialValue: snapshot.data["title"],
+                  style: _fieldStyle,
+                  decoration: _buidlDecoration("Titulo"),
+                  onSaved: (t){},
+                  // ignore: missing_return
+                  validator: (t){},
+                ),
+                TextFormField(
+                  initialValue: snapshot.data["description"],
+                  style: _fieldStyle,
+                  maxLines: 6,
+                  decoration: _buidlDecoration("Descrição"),
+                  onSaved: (t){},
+                  // ignore: missing_return
+                  validator: (t){},
+                ),
+                TextFormField(
+                  initialValue: snapshot.data["price"]?.toStringAsFixed(2),
+                  style: _fieldStyle,
+                  decoration: _buidlDecoration("Preço"),
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  onSaved: (t){},
+                  // ignore: missing_return
+                  validator: (t){},
+                ),
 
-          ],
+              ],
+            );
+          }
         ),
       ),
     );
