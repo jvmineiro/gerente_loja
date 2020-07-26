a20a882fe6a2b6057fa6bb1bc20fe1332d3ad231
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2020-07-26 12:16:03 -0300

    criando um campo para imagens

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -63,6 +63,14 @@ class _ProductScreenState extends State<ProductScreen> {
             return ListView(
               padding: EdgeInsets.all(16),
               children: <Widget>[
+                Text(
+                    "Imagens",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 12
+                  ),
+                ),
+                ImagesWidget(),
                 TextFormField(
                   initialValue: snapshot.data["title"],
                   style: _fieldStyle,
