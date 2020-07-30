91c7478450a2ce8cce9c36c23c721de0fc1ca19d
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2020-07-30 13:10:42 -0300

    integraçao feita com o firebase

--- lib/blocs/product_bloc.dart
+++ lib/blocs/product_bloc.dart
@@ -1,15 +1,17 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:rxdart/rxdart.dart';
 
 class ProductBloc extends BlocBase{
 
   final _dataController = BehaviorSubject<Map>();
-
   final _loadingController = BehaviorSubject<bool>();
+  final _createdController = BehaviorSubject<bool>();
 
   Stream<Map> get outData => _dataController.stream;
   Stream<bool> get outLoading => _loadingController.stream;
+  Stream<bool> get outCreated => _createdController.stream;
 
    String categoryId;
    DocumentSnapshot product;
@@ -21,10 +23,14 @@ class ProductBloc extends BlocBase{
       unsavedData = Map.of(product.data);
       unsavedData["iamges"] = List.of(product.data["images"]);
       unsavedData["sizes"] = List.of(product.data["sizes"]);
+
+      _createdController.add(true);
     } else {
       unsavedData = {
         "title": null, "description": null, "price": null, "images": [],"sizes": [],
       };
+
+      _createdController.add(false);
     }
     
     _dataController.add(unsavedData);
@@ -48,9 +54,44 @@ class ProductBloc extends BlocBase{
 
   Future<bool>  saveProduct() async{
      _loadingController.add(true);
-     await Future.delayed(Duration(seconds: 3));
-     _loadingController.add(false);
-     return true;
+
+     try{
+        if(product != null){
+          await _uploadImages(product.documentID);
+          await product.reference.updateData(unsavedData);
+        } else {
+          DocumentReference dr = await Firestore.instance.collection("products").document(categoryId).
+            collection("itens").add(Map.from(unsavedData)..remove("images"));
+          await _uploadImages(dr.documentID);
+          await dr.updateData(unsavedData);
+        }
+
+        _createdController.add(true);
+        _loadingController.add(false);
+        return true;
+     } catch (e){
+       _loadingController.add(false);
+       return false;
+     }
+  }
+
+  Future _uploadImages(String productId) async {
+     for(int i = 0; i < unsavedData["images"].length; i++){
+       if(unsavedData["images"][i] is String) continue;
+
+       StorageUploadTask uploadTask = FirebaseStorage.instance.ref().child(categoryId).
+        child(productId).child(DateTime.now().millisecondsSinceEpoch.toString()).
+        putFile(unsavedData["images"][i]);
+
+       StorageTaskSnapshot s = await uploadTask.onComplete;
+       String downloadUrl = await s.ref.getDownloadURL();
+
+       unsavedData["images"][i] = downloadUrl;
+     }
+  }
+
+  void deleteProduct(){
+     product.reference.delete();
   }
 
   @override
@@ -58,6 +99,7 @@ class ProductBloc extends BlocBase{
     // TODO: implement dispose
     _dataController.close();
     _loadingController.close();
+    _createdController.close();
   }
 
 }
--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -47,11 +47,34 @@ class _ProductScreenState extends State<ProductScreen> with ProductValidator{
       backgroundColor: Colors.grey[850],
       appBar: AppBar(
         elevation: 0,
-        title: Text("Criar Produto"),
+        title: StreamBuilder<bool>(
+          stream: _productBloc.outCreated,
+          initialData: false,
+          builder: (context, snapshot) {
+            return Text(snapshot.data ? "Editar produto" : "Criar Produto");
+          }
+        ),
         actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.remove),
-            onPressed: (){},
+          StreamBuilder<bool>(
+            stream: _productBloc.outCreated,
+            initialData: false,
+            builder: (context, snapshot) {
+              if (snapshot.data)
+                return StreamBuilder<bool>(
+                  stream: _productBloc.outLoading,
+                  initialData: false,
+                  builder: (context, snapshot) {
+                    return IconButton(
+                      icon: Icon(Icons.remove),
+                      onPressed: snapshot.data ? null : (){
+                        _productBloc.deleteProduct();
+                        Navigator.of(context).pop();
+                      },
+                    );
+                  }
+                );
+              else return Container();
+            },
           ),
           StreamBuilder<bool>(
             stream: _productBloc.outLoading,
