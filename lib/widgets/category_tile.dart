a0088716d3ad38a84ceda380c75ef6a28a38d983
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2020-08-06 15:20:26 -0300

    ...

--- lib/widgets/category_tile.dart
+++ lib/widgets/category_tile.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:gerenteloja/screens/product_screen.dart';
+import 'package:gerenteloja/widgets/edit_category_dialog.dart';
 
 class CategoryTile extends StatelessWidget {
 
@@ -14,9 +15,16 @@ class CategoryTile extends StatelessWidget {
       margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
       child: Card(
         child: ExpansionTile(
-          leading: CircleAvatar(
-            backgroundImage: NetworkImage(category.data["icon"]),
-            backgroundColor: Colors.transparent,
+          leading: GestureDetector(
+            onTap: (){
+              showDialog(context: context,
+              builder: (context) => EditCategoryDialog()
+              );
+            },
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(category.data["icon"]),
+              backgroundColor: Colors.transparent,
+            ),
           ),
           title: Text(
             category.data["title"],
